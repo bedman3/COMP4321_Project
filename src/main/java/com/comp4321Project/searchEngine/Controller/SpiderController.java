50085afe9dc4a515ea9acd23e66a55f57d188362
Martin Wong <martinwong327@gmail.com>
2020-04-01 20:49:37 +0800

    feat: added spider controller

---
+++ src/main/java/com/comp4321Project/searchEngine/Controller/SpiderController.java
@@ -0,0 +1,4 @@
+package com.comp4321Project.searchEngine.Controller;
+
+public class SpiderController {
+}
--- src/main/java/com/comp4321Project/searchEngine/Service/Spider.java
+++ src/main/java/com/comp4321Project/searchEngine/Service/Spider.java
@@ -1,5 +1,7 @@
 package com.comp4321Project.searchEngine.Service;
 
 public class Spider {
+    private String url;
+
 
 }
