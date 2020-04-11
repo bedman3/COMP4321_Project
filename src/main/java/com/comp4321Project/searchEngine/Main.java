9393e79707492b9fc8ac891202df54e82abf512f
Martin Wong <martinwong327@gmail.com>
2020-04-12 03:22:35 +0800

    feat: finish child and parent graph path indexing

--- src/main/java/com/comp4321Project/searchEngine/Main.java
+++ src/main/java/com/comp4321Project/searchEngine/Main.java
@@ -1,7 +1,8 @@
 package com.comp4321Project.searchEngine;
 
-import org.jsoup.nodes.Document;
+import org.apache.tomcat.util.buf.StringUtils;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.rocksdb.Options;
@@ -9,8 +10,6 @@ import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -28,6 +27,7 @@ public class Main {
 
 	// my playground function for doing some RAD
 	public static void main(String[] arg) throws IOException {
+		char separator = ' ';
 
 //		String dbPath = "rocksdbFiles";
 		String websiteMetaDbPath = "rocksDbFiles/WebsiteMetaData/";
@@ -61,15 +61,51 @@ public class Main {
 				} else if (link.startsWith("#")) {
 					// ignore any links start with element link #
 					continue;
+				} else if (link.equals(url)) {
+					// do not create self loop
+					continue;
+				} else {
+					// if urls not under url, then we ignore the outgoing links
+					// e.g. if url == http://www.cse.ust.hk, we will ignore
+					// any link that does not start with http://www.cse.ust.hk
+					continue;
 				}
+
 				links.add(link);
 			}
 
-			for (String link : links) {
-				System.out.println(link);
-			}
 
-//			rocksDB.put(url.getBytes(), links);
+
+			// index child links, index in the format of: key = child_{parent_link}, value = {child_link}
+			// the spider will overwrite the key-value pair in rocksdb because parent -> child paths are
+			// meant to be overwrote if there is update
+			String childLinkKey = String.format("child_%s", url);
+			byte[] test = rocksDBSiteMapData.get(childLinkKey.getBytes());
+			if (test != null) {
+				System.out.println(new String(test));
+			} else {
+				System.out.println(test);
+			}
+			rocksDBSiteMapData.put(childLinkKey.getBytes(), StringUtils.join(links, separator).getBytes());
+
+			// index parent links, index in the format of: key = parent_{child_link}, value = {parent_link}
+			for (String link : links) {
+				String parentLinkKey = String.format("parent_%s", link);
+				byte[] parentLinkValueByte = rocksDBSiteMapData.get(parentLinkKey.getBytes());
+
+				if (parentLinkValueByte == null) {
+					rocksDBSiteMapData.put(parentLinkKey.getBytes(), url.getBytes());
+				} else {
+					String parentLinkValue = new String(parentLinkValueByte);
+					if (parentLinkValue.contains(url)) {
+						// do nothing
+					} else {
+						// append in the end
+						parentLinkValue += String.format("%c%s", separator, url);
+						rocksDBSiteMapData.put(parentLinkKey.getBytes(), parentLinkValue.getBytes());
+					}
+				}
+			}
 
 		} catch (RocksDBException e) {
 			System.err.println(e.toString());
---
+++ src/main/java/com/comp4321Project/searchEngine/Util/RocksDBUtil.java
@@ -0,0 +1,15 @@
+package com.comp4321Project.searchEngine.Util;
+
+import org.apache.tomcat.util.buf.StringUtils;
+
+import java.util.List;
+
+public class RocksDBUtil {
+//    public static String fromListToString(List<String> list) {
+//        return
+//    }
+
+//    public static List<String> fromStringToList() {
+//
+//    }
+}
