479f62debfb637eadb5fbadf9078d8794ca3a3a2
Martin Wong <martinwong327@gmail.com>
2020-04-12 13:42:52 +0800

    feat: added stop words and list of words from doc

--- src/main/java/com/comp4321Project/searchEngine/Main.java
+++ src/main/java/com/comp4321Project/searchEngine/Main.java
@@ -1,6 +1,6 @@
 package com.comp4321Project.searchEngine;
 
-import org.apache.tomcat.util.buf.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,112 +14,142 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
 public class Main {
 
-	private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 //	public static void main(String[] args) {
 //		SpringApplication.run(Main.class, args);
 //	}
 
-	// my playground function for doing some RAD
-	public static void main(String[] arg) throws IOException {
-		char separator = ' ';
+    // my playground function for doing some RAD
+    public static void main(String[] arg) throws IOException {
+        String[] stopWords = {
+        		"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
+                "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers",
+                "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves",
+                "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are",
+                "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does",
+                "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until",
+                "while", "of", "at", "by", "for", "with", "about", "against", "between", "into",
+                "through", "during", "before", "after", "above", "below", "to", "from", "up", "down",
+                "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here",
+                "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more",
+                "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so",
+                "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"
+        };
+
+        char separator = ' ';
 
 //		String dbPath = "rocksdbFiles";
-		String websiteMetaDbPath = "rocksDbFiles/WebsiteMetaData/";
-		String vSpaceModelIndexDbPath = "rocksDbFiles/VSpaceModelIndexData/";
-		String siteMapDbPath = "rocksDbFiles/SiteMapData";
+        String websiteMetaDbPath = "rocksDbFiles/WebsiteMetaData/";
+        String vSpaceModelIndexDbPath = "rocksDbFiles/VSpaceModelIndexData/";
+        String siteMapDbPath = "rocksDbFiles/SiteMapData";
 
-		RocksDB.loadLibrary();
+        RocksDB.loadLibrary();
 
-		try {
-			// The Options class contains a set of configurable DB options
-			// that determines the behaviour of the database.
-			Options options = new Options();
-			options.setCreateIfMissing(true);
+        try {
+            // The Options class contains a set of configurable DB options
+            // that determines the behaviour of the database.
+            Options options = new Options();
+            options.setCreateIfMissing(true);
 
-			RocksDB rocksDBWebsiteMetaData = RocksDB.open(options, websiteMetaDbPath);
-			RocksDB rocksDBVSpaceModelIndexData = RocksDB.open(options, vSpaceModelIndexDbPath);
-			RocksDB rocksDBSiteMapData = RocksDB.open(options, siteMapDbPath);
+            RocksDB rocksDBWebsiteMetaData = RocksDB.open(options, websiteMetaDbPath);
+            RocksDB rocksDBVSpaceModelIndexData = RocksDB.open(options, vSpaceModelIndexDbPath);
+            RocksDB rocksDBSiteMapData = RocksDB.open(options, siteMapDbPath);
 
-			String url = "http://www.cse.ust.hk";
-			Set<String> links = new HashSet<String>();
+            String url = "http://www.cse.ust.hk";
+            Set<String> links = new HashSet<String>();
 
-			Connection.Response response = Jsoup.connect(url).execute();
-			// extract last modified from response header
-			String lastModified = response.header("Last-Modified");
-			if (lastModified == null) {
-				lastModified = response.header("Date");
-			}
-			System.out.println(lastModified);
+            Connection.Response response = Jsoup.connect(url).execute();
+            // extract last modified from response header
+            String lastModified = response.header("Last-Modified");
+            if (lastModified == null) {
+                lastModified = response.header("Date");
+            }
 
-			Document doc = response.parse();
-			Elements linkElements = doc.select("a[href]");
-			for (Element linkElement : linkElements) {
-				String link = linkElement.attr("href");
-				// it will contain links like /admin/qa/
-				// we will need to append the parent url to the relative url
-				// so the result will become http://www.cse.ust.hk/admin/qa/
-				if (link.startsWith("/")) {
-					link = url + link;
-				} else if (link.startsWith("#")) {
-					// ignore any links start with element link #
-					continue;
-				} else if (link.equals(url)) {
-					// do not create self loop
-					continue;
-				} else {
-					// if urls not under url, then we ignore the outgoing links
-					// e.g. if url == http://www.cse.ust.hk, we will ignore
-					// any link that does not start with http://www.cse.ust.hk
-					continue;
-				}
-				links.add(link);
-			}
+            String size = response.header("Content-Length");
 
-			// store title
-			Elements titleElements = doc.select("title");
-			String title = titleElements.first().text();
+            Document doc = response.parse();
+            Elements linkElements = doc.select("a[href]");
+            String docText = doc.text();
+            for (Element linkElement : linkElements) {
+                String link = linkElement.attr("href");
+                // it will contain links like /admin/qa/
+                // we will need to append the parent url to the relative url
+                // so the result will become http://www.cse.ust.hk/admin/qa/
+                if (link.startsWith("/")) {
+                    link = url + link;
+                } else if (link.startsWith("#")) {
+                    // ignore any links start with element link #
+                    continue;
+                } else if (link.equals(url)) {
+                    // do not create self loop
+                    continue;
+                } else {
+                    // if urls not under url, then we ignore the outgoing links
+                    // e.g. if url == http://www.cse.ust.hk, we will ignore
+                    // any link that does not start with http://www.cse.ust.hk
+                    continue;
+                }
+                links.add(link);
+            }
 
+            // tokenize the words
+            String[] wordsArray = StringUtils.split(docText, " ");
+            System.out.println(Arrays.toString(wordsArray));
 
+            // store title
+//			Elements titleElements = doc.select("title");
+            String title = doc.title();
 
-			String metaKey = String.format("meta_%s", url);
+            // extract the size
+            if (size == null) {
+                // remove all whitespaces and get the length = number of characters
+                int length = docText.replaceAll("\\s+", "").length();
+                size = Integer.toString(length) + "characters";
+            } else {
+                size += "bytes";
+            }
+
+            String metaKey = String.format("meta_%s", url);
+            // use a separator that will rarely appear in the title
+//			String metaValue = String.format("%s |,.| %s |,.| %s |,.| %s |,.| %s |,.| %s |,.| %s", title, url, lastModified, size, keyFreq, parentLinks, childLinks)
 //			rocksDBWebsiteMetaData.put(titleKey.getBytes(), title.getBytes());
 
 
+            // index child links, index in the format of: key = child_{parent_link}, value = {child_link}
+            // the spider will overwrite the key-value pair in rocksdb because parent -> child paths are
+            // meant to be overwrote if there is update
+            String childLinkKey = String.format("child_%s", url);
+            rocksDBSiteMapData.put(childLinkKey.getBytes(), StringUtils.join(links, separator).getBytes());
 
-			// index child links, index in the format of: key = child_{parent_link}, value = {child_link}
-			// the spider will overwrite the key-value pair in rocksdb because parent -> child paths are
-			// meant to be overwrote if there is update
-			String childLinkKey = String.format("child_%s", url);
-			rocksDBSiteMapData.put(childLinkKey.getBytes(), StringUtils.join(links, separator).getBytes());
+            // index parent links, index in the format of: key = parent_{child_link}, value = {parent_link}
+            for (String link : links) {
+                String parentLinkKey = String.format("parent_%s", link);
+                byte[] parentLinkValueByte = rocksDBSiteMapData.get(parentLinkKey.getBytes());
 
-			// index parent links, index in the format of: key = parent_{child_link}, value = {parent_link}
-			for (String link : links) {
-				String parentLinkKey = String.format("parent_%s", link);
-				byte[] parentLinkValueByte = rocksDBSiteMapData.get(parentLinkKey.getBytes());
-
-				if (parentLinkValueByte == null) {
-					rocksDBSiteMapData.put(parentLinkKey.getBytes(), url.getBytes());
-				} else {
-					String parentLinkValue = new String(parentLinkValueByte);
-					if (parentLinkValue.contains(url)) {
-						// do nothing
-					} else {
-						// append in the end
-						parentLinkValue += String.format("%c%s", separator, url);
-						rocksDBSiteMapData.put(parentLinkKey.getBytes(), parentLinkValue.getBytes());
-					}
-				}
-			}
-		} catch (RocksDBException e) {
-			System.err.println(e.toString());
-		}
-	}
+                if (parentLinkValueByte == null) {
+                    rocksDBSiteMapData.put(parentLinkKey.getBytes(), url.getBytes());
+                } else {
+                    String parentLinkValue = new String(parentLinkValueByte);
+                    if (parentLinkValue.contains(url)) {
+                        // do nothing
+                    } else {
+                        // append in the end
+                        parentLinkValue += String.format("%c%s", separator, url);
+                        rocksDBSiteMapData.put(parentLinkKey.getBytes(), parentLinkValue.getBytes());
+                    }
+                }
+            }
+        } catch (RocksDBException e) {
+            System.err.println(e.toString());
+        }
+    }
 
 }
